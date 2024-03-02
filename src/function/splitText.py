8e1c51f55b4f43b8187299fcd5292add071d14ab
Nimono-sleep-well <monjitto.1119@outlook.jp>
2024-03-02 21:34:53 +0900

    Oshikari_DM
    暴言を吐くとおぢさんがお叱りのDMを入れます

--- src/function/splitText.py
+++ src/function/splitText.py
@@ -10,13 +10,20 @@ splitted_text: str = []
 def split_text(msg):
     splitted_text = []
 
-    splitted_text = []
-
     tagger = MeCab.Tagger("-Ochasen")
 
     words = [line for line in tagger.parse(msg).splitlines() if "名詞" in line.split()[-1]]
 
     for i in words:
         splitted_text.append(i.split()[0])
+
+    return splitted_text
+
+def split_all(msg):
+    splitted_text = []
+
+    tagger = MeCab.Tagger("-Owakati")
+
+    splitted_text = tagger.parse(msg).split()
 
     return splitted_text
