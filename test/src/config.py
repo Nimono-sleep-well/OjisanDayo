7f2ec65bc986a20527621d191e94171a14443dd8
Nimono-sleep-well <monjitto.1119@outlook.jp>
2024-03-02 15:52:27 +0900

    test

---
+++ test/src/config.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
---
+++ test/src/function/splitText.py
@@ -0,0 +1,19 @@
+import random
+import MeCab
+
+reactionState: bool
+
+words: str = []
+
+splitted_text: str = []
+
+def split_text(msg):
+
+    tagger = MeCab.Tagger("-Ochasen")
+
+    words = [line for line in tagger.parse(msg).splitlines() if "名詞" in line.split()[-1]]
+
+    for i in words:
+        splitted_text.append(i.split()[0])
+
+    return splitted_text
