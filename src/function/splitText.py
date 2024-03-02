765beccf079debf5c880e73147e4891f7fc7822e
c-6p <106654790+c-6p@users.noreply.github.com>
2024-03-02 19:47:51 +0900

    known_wordの修正と絵文字の追加

--- src/function/splitText.py
+++ src/function/splitText.py
@@ -9,6 +9,8 @@ splitted_text: str = []
 
 def split_text(msg):
 
+    splitted_text = []
+
     tagger = MeCab.Tagger("-Ochasen")
 
     words = [line for line in tagger.parse(msg).splitlines() if "名詞" in line.split()[-1]]
